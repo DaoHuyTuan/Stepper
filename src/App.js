76af79a6e724e3aaa3609b2c2705d7126164894d
Tuan Dao <tuandh95@gmail.com>
2020-02-29 00:49:32 +0700

    finish version 2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React, { useState } from 'react';
-import "./styles.scss"
-import Stepper from './components/Stepper';
-import { stepList }  from "./stepList";
-import Step from './components/StepItem';
-const App = React.memo(props => {
-  const [listStep, setListStep] = useState(stepList);
+import React, { useState } from "react";
+import Stepper from "./components/Stepper";
+import Step from "./components/StepItem";
+const App = React.memo(() => {
+  const [currentStep, setCurrentStep] = useState(1);
   const handleStepChange = id => {
-     const cloneListStep = JSON.parse(JSON.stringify(stepList));
-     cloneListStep.map(item => {
-  
-        if(item.id === id) {
-          item.clssName = "activated";
-        }
-        else {
-          item.clssName = "";
-        }
-        if(item.id < id) {
-          item.clssName = "visited";
-        }
-
-        return item
-     })
-     setListStep(cloneListStep)
-  }
+    setCurrentStep(id);
+  };
   return (
     <div className="App">
-      <Stepper>
-         {listStep.map(item => {
-           return <Step title={item.label} order={item.order} clssName={item.clssName} key={item.id} handleStepChange={() => handleStepChange(item.id)}/>
-         })}
+      <Stepper handleStepChange={handleStepChange} currentActive={currentStep}>
+        <Step title="Supplier" order={1} activeKey={1} />
+        <Step title="Basic Infomation" order={2} activeKey={2} />
+        <Step title="Language" order={3} activeKey={3} />
+        <Step title="Lorem ispsum" order={4} activeKey={4} />
+        <Step title="Some dumb text" order={5} activeKey={5} />
+        <Step title="Vertical Stepper" order={6} activeKey={6} />
       </Stepper>
     </div>
   );
-})
+});
 
 export default App;
